c6462ee04743b9146b60675851fc17a40fa055d8
Adriel Puello <adrielpuello@gmail.com>
2022-08-29 11:28:15 -0400

    commit tree a773affed84596f0b794422bad83b182765d12b7

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your code in this file!
+//currentUser
+//      1) is defined
+//    welcomeMessage
+//      2) contains "Welcome to Flatbook, "
+//      3) contains the value of the 'currentUser' variable
+//      4) ends with an exclamation point!
+//    excitedWelcomeMessage
+//      5) contains "WELCOME TO FLATBOOK, "
+//      6) contains the value of the 'currentUser' variable
+//      7) ends with an exclamation point
+//    shortGreeting
+//      8) contains "Welcome, "
+//      9) contains the first initial of the name stored in the 'currentUser' variable
+//      10) ends with an exclamation point
+const currentUser = "Adriel Puello"
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
+const upperCaseCurrentUser = currentUser.toUpperCase();
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${upperCaseCurrentUser}!`
+const shortUser = "A"
+const shortGreeting = `Welcome, ${shortUser}!`
